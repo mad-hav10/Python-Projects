1c92b2eb7a8269a5612aea6f120dbb506c4691bd
Madhav Lalit <147257829+mad-hav10@users.noreply.github.com>
2025-01-16 15:49:16 +0530

    Update scoreboard.py

--- Project-018/scoreboard.py
+++ Project-018/scoreboard.py
@@ -7,6 +7,8 @@ class ScoreBoard(Turtle) :
     def __init__(self) :
         super().__init__()
         self.score = 0
+        with open(r"C:\Users\sodiu\OneDrive\Documents\Coding\Python Projects\Project-018\data.txt") as data :
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -14,13 +16,17 @@ class ScoreBoard(Turtle) :
         self.hideturtle()
         
     def update_scoreboard(self) :
-        self.write(f"Score: {self.score}",align=ALLIGNMENT, font=FONT)
-
+        self.clear()
+        self.write(f"Score: {self.score} HighScore: {self.highscore}",align=ALLIGNMENT, font=FONT)
+        
     def increase_score(self) :
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self) :
-        self.goto(0, 0)
-        self.write(f"Game Over",align=ALLIGNMENT, font=FONT)
+    def reset(self) :
+        if self.score > self.highscore :
+            self.highscore = self.score
+            with open(r"C:\Users\sodiu\OneDrive\Documents\Coding\Python Projects\Project-018\data.txt", "w") as data :
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
